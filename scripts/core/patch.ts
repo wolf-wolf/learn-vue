33eff8821d3db3c71706161512e65ef93e972bad
hunter.li <liming_pr@163.com>
2021-06-17 22:02:28 +0800

    feat: 添加 patchPortal

--- scripts/core/patch.ts
+++ scripts/core/patch.ts
@@ -84,6 +84,38 @@ function patchFragment(prevVNode: VNode<any>, nextVNode: VNode<any>, container: 
 }
 
 function patchPortal(prevVNode: VNode<any>, nextVNode: VNode<any>) {
+    patchChildren(
+        prevVNode.childFlags,
+        nextVNode.childFlags,
+        prevVNode.children,
+        nextVNode.children,
+        prevVNode.tag as any // 注意容器元素是旧的 container
+    )
+
+    // 让 nextVNode.el 指向 prevVNode.el
+    nextVNode.el = prevVNode.el;
+
+    // 如果新旧容器不同，才需要搬运
+    if (nextVNode.tag !== prevVNode.tag) {
+        // 获取新的容器元素，即挂载目标
+        const container = (typeof nextVNode.tag === 'string' ? document.querySelector(nextVNode.tag) : nextVNode.tag) as any;
+
+        switch (nextVNode.childFlags) {
+            case ChildrenFlags.SINGLE_VNODE:
+                // 如果新的 Portal 是单个子节点，就把该节点搬运到新容器中
+                container.appendChild(nextVNode.children.el)
+                break
+            case ChildrenFlags.NO_CHILDREN:
+                // 新的 Portal 没有子节点，不需要搬运
+                break
+            default:
+                // 如果新的 Portal 是多个子节点，遍历逐个将它们搬运到新容器中
+                for (let i = 0; i < nextVNode.children.length; i++) {
+                    container.appendChild(nextVNode.children[i].el)
+                }
+                break
+        }
+    }
 }
 
 export function patch(prevVNode: VNode<any>, nextVNode: VNode<any>, container: Element) {
